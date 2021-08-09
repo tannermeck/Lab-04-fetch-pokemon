77ce64248ac1be203b494cdbc1cb08202cf0f3f0
TANNER MECK <steve.meck246@gmail.com>
2021-08-09 16:52:30 -0700

    assignment completed

--- src/Home.js
+++ src/Home.js
@@ -14,6 +14,7 @@ class Home extends Component {
                         <li>Search for your favorite pokemon by name</li>
                         <li>Sort in ascending or descending order</li>
                         <li>Click the image to view the details of the selected pokemon</li>
+                        <li>View up to 20 pokemon per page and click the next and previous buttons to catch them all</li>
                     </ul>
                 </div>
             </section>
